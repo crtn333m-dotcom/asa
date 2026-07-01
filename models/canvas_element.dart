24c5e00576903f7e75bcf51dc7c675a26541392c
crtn333m-dotcom <crtn333m@gmail.com>
2026-07-01 03:32:45 +0300

    Add CanvasElement class with serialization

---
+++ models/canvas_element.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+
+
+enum ElementType{
+
+trunk,
+branch,
+leaf
+
+}
+
+
+
+class CanvasElement{
+
+
+String id;
+
+
+ElementType type;
+
+
+Offset position;
+
+
+double width;
+
+double height;
+
+
+double rotation;
+
+
+Color color;
+
+
+bool locked;
+
+
+String text;
+
+
+
+int layer;
+
+
+
+CanvasElement({
+
+required this.id,
+
+required this.type,
+
+required this.position,
+
+
+this.width=100,
+
+this.height=120,
+
+
+this.rotation=0,
+
+
+this.color=Colors.brown,
+
+
+this.locked=false,
+
+
+this.text="",
+
+
+this.layer=0
+
+});
+
+
+
+Map<String,dynamic> toJson(){
+
+return {
+
+
+"id":id,
+
+"type":type.name,
+
+
+"x":position.dx,
+
+
+"y":position.dy,
+
+
+"width":width,
+
+
+"height":height,
+
+
+"rotation":rotation,
+
+
+"text":text
+
+
+};
+
+
+}
+
+
+}
